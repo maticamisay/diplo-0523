90e0da24a783680e0bcdcf224894cde0a2ab3a51
maticamisay <maticamisay99@hotmail.com>
2024-02-26 19:06:49 -0300

    frontend 1: clase 1

--- 5-FRONTEND 1/Clase1/index.js
+++ 5-FRONTEND 1/Clase1/index.js
@@ -21,7 +21,62 @@ buttonLogin.innerHTML = "Iniciar sesión";
 buttonLogin.style.backgroundColor = "black";
 buttonLogin.style.color = "white";
 
+const clickLogin = () => {
+  window.location.replace("/login.html");
+};
+buttonLogin.addEventListener("click", clickLogin);
+
 navbar.appendChild(buttonLogin);
 
 const titleH1 = document.querySelector("main h1");
 titleH1.style.color = "white";
+
+const botonAzul = document.querySelector("#azul");
+botonAzul.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = "blue";
+});
+const botonReset = document.querySelector("#reset");
+botonReset.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = "white";
+});
+
+const productos = [
+  {
+    id: 1,
+    nombre: "disco",
+  },
+  {
+    id: 2,
+    nombre: "celular",
+  },
+  {
+    id: 3,
+    nombre: "impresora",
+  },
+  {
+    id: 4,
+    nombre: "tinta",
+  },
+  {
+    id: 5,
+    nombre: "tablet",
+  },
+];
+
+const mainNodo = document.querySelector("main");
+productos.forEach((prod) => {
+  const divProducto = document.createElement("div");
+  const titleProducto = document.createElement("h3");
+  const idProducto = document.createElement("span");
+
+  titleProducto.innerHTML = prod.nombre;
+  idProducto.innerHTML = `Id ${prod.id}`;
+  divProducto.appendChild(titleProducto);
+  divProducto.appendChild(idProducto);
+  divProducto.style.border = "1px solid black";
+  divProducto.style.marginBottom = "5px";
+
+  mainNodo.appendChild(divProducto);
+});
