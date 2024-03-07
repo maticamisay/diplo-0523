0cab314db673dfbc51faa77f095fb7f2330fda79
maticamisay <maticamisay99@hotmail.com>
2024-03-07 01:00:09 -0300

    frontend 1: clase 5

---
+++ 5-FRONTEND 1/Clase5/main.js
@@ -0,0 +1,47 @@
+const getProducts = async () => {
+  const resposne = await fetch("https://fakestoreapi.com/products");
+  const data = await resposne.json();
+  return data;
+};
+
+const saveCartLocalStorage = (item) => {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const existingProductIndex = cart.findIndex(
+    (product) => product.id === item.id
+  );
+  if (existingProductIndex !== -1) {
+    cart[existingProductIndex].quantity =
+      (cart[existingProductIndex].quantity || 1) + 1;
+  } else {
+    item.quantity = 1;
+    cart.push(item);
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+const printProducts = async () => {
+  const products = await getProducts();
+  const productsContainer = document.querySelector("#products");
+
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+        <img src="${product.image}" alt="${product.title}" />
+        <h2>${product.title}</h2>
+        <h3>$ ${product.price}</h3>
+        <p>${product.id}</p>
+        `;
+    const button = document.createElement("button");
+    button.textContent = "Agregar al carrito";
+    button.addEventListener("click", () => {
+      saveCartLocalStorage(product);
+    });
+    productDiv.appendChild(button);
+    productsContainer.appendChild(productDiv);
+  });
+};
+
+printProducts();
