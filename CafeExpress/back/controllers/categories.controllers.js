4f5add70a1a735f260f5e00a494a2f62ae20bf68
maticamisay <maticamisay99@hotmail.com>
2024-04-22 20:17:00 -0300

    agregamos getCategoryById, updateCategory  y deleteCategory

--- CafeExpress/back/controllers/categories.controllers.js
+++ CafeExpress/back/controllers/categories.controllers.js
@@ -12,24 +12,75 @@ const getAllCategories = async (req, res) => {
   }
 };
 
-const getCategoryById = async (req, res) => {};
-
-const createCategory = async (req, res) => {
+const getCategoryById = async (req, res) => {
   try {
-    const { nombre } = req.body;
-    const newCategory = await Category.create({ category_name: nombre });
-    return res.status(201).json({ data: newCategory });
+    const { idCategoria } = req.params;
+    const category = await Category.findByPk(idCategoria);
+    return res.json(category);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Error inesperado en el servidor" });
   }
 };
 
-const updateCategory = async (req, res) => {};
+const createCategory = async (req, res) => {
+  try {
+    const { nombre } = req.body;
+    const newCategory = await Category.create({ category_name: nombre });
+    return res
+      .status(201)
+      .json({ message: "Categoria creada correctamente", data: newCategory });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error inesperado en el servidor" });
+  }
+};
 
-const deleteCategory = async (req, res) => {};
+const updateCategory = async (req, res) => {
+  const oldCategory = req.body;
+  const { idCategoria } = req.params;
+
+  try {
+    await Category.update(
+      {
+        category_name: oldCategory.nombre,
+      },
+      {
+        where: {
+          id: idCategoria,
+        },
+      }
+    );
+    return res.json({
+      message: "Categoria actualizada correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error inesperado en el servidor" });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  const { idCategoria } = req.params;
+  try {
+    await Category.destroy({
+      where: {
+        id: idCategoria,
+      },
+    });
+    return res.json({
+      message: "Categoria eliminada correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error inesperado en el servidor" });
+  }
+};
 
 module.exports = {
   getAllCategories,
+  getCategoryById,
   createCategory,
+  updateCategory,
+  deleteCategory,
 };
--- CafeExpress/back/routes/categories.routes.js
+++ CafeExpress/back/routes/categories.routes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const {
   getAllCategories,
   createCategory,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
 } = require("../controllers/categories.controllers");
 const router = express.Router();
 
 router.get("/", getAllCategories);
+router.get("/:idCategoria", getCategoryById);
 router.post("/", createCategory);
+router.put("/:idCategoria", updateCategory);
+router.delete("/:idCategoria", deleteCategory);
 
 module.exports = router;
